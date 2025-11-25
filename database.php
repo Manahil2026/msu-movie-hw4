fc4c3c5db802180b7083a886c33ed98a3d6121ab
Manahil2026 <manahil.im111@gmail.com>
2025-11-25 13:46:40 -0500

    add comment

--- database.php
+++ database.php
@@ -1,7 +1,8 @@
 <?php
+    // This page is done by Manahil Imran
     // Establish a databse connection
     // Copied from the instrcutions given by professor
-    
+
     $dsn = 'mysql:host=localhost;dbname=MSU_Movies';
     $username = 'mgs_user';
     $password = 'pa55word';
